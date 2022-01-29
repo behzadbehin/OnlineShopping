446ebd3b3db4bb1cad0107a60103ec81682cbe10
Behzad Behin <b.behin@nomatec.net>
2022-01-29 15:23:38 +0330

    .

---
+++ OnlineShopping.Core/Entity/Entity.cs
@@ -0,0 +1,7 @@
+namespace OnlineShopping.Core.Entity
+{
+    public class Entity
+    {
+        public virtual Guid Id { get; set; }
+    }
+}
---
+++ OnlineShopping.Domain/Models/Product.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using OnlineShopping.Core.Entity;
+
+namespace OnlineShopping.OrderContext
+{
+    public class Product : Entity
+    {
+        [Required]
+        public virtual string Name { get; set; }
+    }
+}
